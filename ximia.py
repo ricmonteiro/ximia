535d5a6f33421bc3f41bdc67438f8f2f818b8784
Ricardo Monteiro <ricardo_luis19@hotmail.com>
2020-12-21 21:29:28 +0000

    Commit

--- ximia.py
+++ ximia.py
@@ -12,7 +12,8 @@ import pandas
 class Ximia(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
-        self.master = master       
+        self.master = master   
+         
 
     def search(self=None, event=None):
         api_key = os.environ.get("CHEMSPI_API_KEY") 
@@ -22,20 +23,16 @@ class Ximia(tk.Frame):
             cs = "Error..."   
         search_item = str(search_box.get())
         global compound
-        result = ["glucose", "ethanol", "triterpenoid"] 
-        result_pch = pch.get_compounds(result[0], "name", record_type="3d")
+        result_pch = pch.get_compounds(search_item, "name", record_type="3d")
         print(result_pch)
-        result_list = tk.Listbox(root, width=15, height=len(result))
-        result_list.grid(row=3, column=0, padx=10, pady=10, sticky='w'+'e'+'n'+'s')        
-        for item in result:
-            result_list.insert(tk.END, item)
+        for compound in cs.search(search_item):
+            compound_name = str(compound.common_name) 
+            result_list = tk.Listbox(root, width=15, height=len(result))
+            result_list.grid(row=3, column=0, columnspan=3, padx=10, pady=10, sticky='w'+'e'+'n'+'s')        
+            result_list.insert(tk.END, str(result_pch[0].iupac_name))
             
-        
-        #for compound in cs.search(search_item):
-        #    compound_name = str(compound.common_name)
-        #    result_list.insert(tk.END, compound_name)
-
-
+            
+              
 # MAIN WINDOW
 
     # Main window options
@@ -46,28 +43,54 @@ root.geometry() # Window size
 root.resizable(width=0, height=0) #Window size and make it resizable in height
 root.iconphoto(True, tk.PhotoImage(file='./icon.png')) # Top left icon
 root.config(bg="#b6d6fd") #Background color
-root.bind("<Return>", Ximia.search) # Bind ENTER key to search function
-    
+
+    # Ximia logo
+#logo = ImageTk.PhotoImage(Image.open()
+#logo.grid(row=0, column=0)
+
 
     # Search box
 
 search_box = tk.Entry(root, width=30) # Create entrytext box for search
 search_box.config(fg="black", font=("Galaxy BT", 24)) # Font color black, style and size
-search_box.grid(row=0, column=0, columnspan=3, padx=10, pady=10) # Show search box
+search_box.grid(row=0, column=1, columnspan=3, padx=10, pady=10) # Show search box
+print("search box created")
+
+    # Check boxes for search type 
+
+chems_search_var = tk.StringVar() 
+check_box_1 = tk.Checkbutton(root, text="Search ChemSpider", variable=chems_search_var,activebackground="#b6d6fd")
+check_box_1.grid(row=0, column=5)
+check_box_1.config(bg="#b6d6fd")
+check_box_1.deselect()
+
+print("checkbox 1 created")
+
+pubchem_search_var = tk.StringVar() 
+check_box_2 = tk.Checkbutton(root, text="Search PubChem", variable=pubchem_search_var, activebackground="#b6d6fd")
+check_box_2.grid(row=0, column=6,padx=10)
+check_box_2.config(bg="#b6d6fd")
+check_box_2.deselect()
+
+print("checkbox 2 created")
 
 
-    # Check boxes for search type
+root.bind("<Return>", Ximia.search) # Bind ENTER key to search function
 
-check_box_1 = tk.Checkbutton(root, text="Search ChemSpider")
-check_box_1.grid(row=0, column=4)
-check_box_2 = tk.Checkbutton(root, text="Search PubChem")
-check_box_2.grid(row=0, column=5)
 
+print("Enter key bind implemented")
 
     # Search button
 
 search_button = tk.Button(root, text="Find your molecule", command=Ximia.search) #Create search button
 search_button.config(fg="black", font=("Galaxy BT", 24)) # Button style and size
-search_button.grid(row=1, column=0, columnspan=3, padx=10, pady=10) #Create search button
+search_button.grid(row=1, column=1, columnspan=3, padx=10, pady=10) #Create search button
+
+print("Search button created")
 ximia = Ximia(master=root)
+
+###tests
 root.mainloop()
+
+
+
