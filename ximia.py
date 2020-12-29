89995000c664fcbf2e2ac0b1e9f9bbdb3a25ed2c
Ricardo Monteiro <ricardo_luis19@hotmail.com>
2020-12-29 11:46:17 +0000

    Restarted code with more structure

--- ximia.py
+++ ximia.py
@@ -6,97 +6,54 @@ from chemspipy import ChemSpider
 import pubchempy as pch
 import os
 
-# Ximia class, the heart of the application with __init__ and the search function
+
+
+class Search(tk.Button):
+    def __init__(self,master=None,text=None):
+        tk.Button.__init__(self,master,text=text)
+        self['command'] = self.search
+
+        tk.Entry.__init__(self, width=60, justify='center')
+        
+
+
+
+    def search(self):
+        print("search made")
+
+# Class for the main frame
+# It shows the app title
+# 
 
 class Ximia(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master   
-         
-    def search(self=None, event=None):
+    def __init__(self,master=None):
+        tk.Frame.__init__(self,master)
+
+        self.labelHello = tk.Label(self,text="Ximia app")
+        self.labelHello['fg'] = "red"
+        self.labelHello.grid()
+
+        self.search_box = Search(self) 
+        self.search_box.config(fg="black", font=("Galaxy BT", 24)) 
+        self.search_box.grid(row=0, column=0, columnspan=3, padx=10, pady=10) 
+
+        self.search_button = Search(self,text="Find your molecule")
+        self.search_button.grid()
 
 
-        search_frame = tk.Frame(root)
-        result_list = tk.Listbox(search_frame, font=("Times New Roman", 20), height=15)
-        sb_y = tk.Scrollbar(search_frame, orient="vertical")
-        result_list.config(yscrollcommand=sb_y.set) 
-        sb_y.config(command=result_list.yview)
-        search_frame.config(bg="#b6d6fd")
-                       
-        #ChemSpider API search
-        api_key = os.environ.get("CHEMSPI_API_KEY") 
-        try:       
-            cs = ChemSpider(api_key)
-        except Exception:
-            print("Error...")   
-        search_item = str(search_box.get())
-        global compound_name
-        for compound in cs.search(search_item):
-            compound_name = str(compound.common_name) 
-        
-        #PubChem API search
-        result_pch = pch.get_cids(search_item, "name", record_type="3d")
-        print(result_pch)
-        results_from_pubchem = pch.Compound.from_cid(result_pch[0]).synonyms[0:20]
-        print(results_from_pubchem)
-        for item in results_from_pubchem:
-            result_list.insert(tk.END,item)
+# main function and main configurations
+# main window is NOT resizable
+# icon placement in top left corner
+# background set to light blue
 
-        try:
-            for item in compound_name:
-                result_list.insert(tk.END, item)
-        except Exception:
-            print("No results were obtained from the ChemSpider APi")
+def main():
+    root = tk.Tk()
+    ximia = Ximia(master=root)
+    ximia.grid() #place ximia main window
+    root.resizable(width=0, height=0) #Window size and make it resizable in height
+    root.iconphoto(True, tk.PhotoImage(file='./icon.png')) # Top left icon
+    root.config(bg="#b6d6fd") #Background color
+    root.mainloop()
 
-        search_label = tk.Label(search_frame, text="Search results", font=("Helvetica", 18))
-        search_label.config(bg="#b6d6fd")
-        search_label.grid()
-        search_frame.grid(row=2, column=0, padx=10, sticky='nsw')
-        result_list.grid(row=1, column=0, pady=10, sticky='nsw')
-        sb_y.grid(row=1, column=4, sticky='nse', pady=10) 
-        print(result_list.curselection())
-        testlabel = tk.Label(root)
-        testlabel.grid(row=2, column=2)
-
-
-        
-    def choose_result():
-                  pass
-                
-    # MAIN WINDOW
-
-    # Main window options
-
-root = tk.Tk() # Create Tk
-root.title("XIMIA") # Window title
-root.geometry() # Window size
-root.resizable(width=0, height=0) #Window size and make it resizable in height
-root.iconphoto(True, tk.PhotoImage(file='./icon.png')) # Top left icon
-root.config(bg="#b6d6fd") #Background color
-
-    # Ximia logo
-#logo = ImageTk.PhotoImage(Image.open()
-#logo.grid(row=0, column=0)
-
-    # Search box
-
-search_box = tk.Entry(root, width=40) # Create entrytext box for search
-search_box.config(fg="black", font=("Galaxy BT", 24)) # Font color black, style and size
-search_box.grid(row=0, column=0, columnspan=3, padx=10, pady=10) # Show search box
-print("search box created")
-
-root.bind("<Return>", Ximia.search) # Bind ENTER key to search function
-
-print("Enter key bind implemented")
-
-    # Search button
-
-search_button = tk.Button(root, text="Find your molecule", command=Ximia.search) #Create search button
-search_button.config(fg="black", font=("Galaxy BT", 24)) # Button style and size
-search_button.grid(row=1, column=0, columnspan=3, padx=10, pady=10) #Create search button
-
-print("Search button created")
-ximia = Ximia(master=root)
-
-###tests
-root.mainloop()
+if __name__ == '__main__':
+    main()
