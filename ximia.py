486f50fb4329add1d6d5265776c545f825e17419
ricmonteiro <ricardo_luis19@hotmail.com>
2021-02-24 01:07:03 +0000

    Image presentation added

--- ximia.py
+++ ximia.py
@@ -2,11 +2,9 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
 from PIL import Image, ImageTk
-import json
-import requests
-from chemspipy import ChemSpider
 import pubchempy as pch
 import os
+import pandas as pd
 
 # Class for the main frame
 # App title
@@ -55,8 +53,11 @@ class Ximia(tk.Frame):
         try:    
             result_pch = pch.get_cids(search_item, 'name', 'substance', list_return='flat')                     
             global results_from_pubchem
+            #print(result_pch)
             results_from_pubchem = [pch.Compound.from_cid(res) for res in result_pch]
+            #print(results_from_pubchem)
             
+
             # Error if there are no results
             if len(results_from_pubchem)==0:
                 self.no_results()
@@ -66,13 +67,17 @@ class Ximia(tk.Frame):
         except:
             results_from_pubchem = []
             self.error()
-      
+            
+        prop_df = pch.compounds_to_frame(results_from_pubchem) 
+
+
     ### FUNCTION THAT WARNS ABOUT NOT GETTING RESULTS ###
     def no_results(self):
         messagebox.showerror(title="No results", message="Your search yielded no results. Please search again with a different word.")
 
     def error(self):
         messagebox.showerror(title="Error", message="Error accessing the PubChem API")
+
 
     ### FUNCTION TO SHOW RESULTS ON MAIN FRAME ###
     def show_results(self, results):
@@ -86,12 +91,19 @@ class Ximia(tk.Frame):
         # Present search results widgets on frame
         self.search_frame.grid(row=3, column=0, padx=10, sticky='nsw')
         self.result_list.grid(row=4, column=0, rowspan=10, pady=10, sticky='nsw')
-        self.result_list.delete(0, tk.END)
-
+        self.result_list.delete(0, tk.END)    
+ 
         # Print a synonym of the searched Compounds onto the results list
         for i in results:
-            print(i)
-            self.result_list.insert(tk.END, i.synonyms[0])                     
+            try:
+                self.result_list.insert(tk.END, i.synonyms[0]) 
+                          
+            except:
+                pass     
+        for i in results:
+            temp_name = str(i.synonyms[0])
+            temp_path = 'images/' + temp_name + '.png'
+            c_img = pch.download('PNG', temp_path, temp_name, 'name', overwrite=True)           
 
         # Mount scroll bar
         self.sb_y.grid(row=4, column=1, rowspan=10, sticky='ns', pady=10) 
@@ -100,20 +112,35 @@ class Ximia(tk.Frame):
 
     ### FUNCTION THAT SHOWS THE DETAILS ON THE CURRENT MOLECULE SELECTED SEARCH RESULTS LISTBOX ###
     def show_result_details(self, event):
+
+        # molecule image
+        img_path = 'images/' + str(self.result_list.get(self.result_list.curselection())) + '.png'
+        print(img_path)
+        self.img = ImageTk.PhotoImage(file=img_path)
+
+        self.canvas = tk.Canvas(self.search_frame)
+        self.canvas.grid(row=4, column=8, columnspan=3)
+
+        self.img_label = tk.Label(self.canvas, image=self.img)
+        self.img_label.pack()  
         
+        # molecular formula label
         self.molecular_formula_label = tk.Label(self.search_frame, font=("Times New Roman", 20), text="Molecular formula: ")
         self.molecular_formula_label.config(bg="#b6d6fd")
         self.molecular_formula_label.grid(row=12, column=8, padx=10, pady=5)
 
-        self.molecular_weight_label = tk.Label(self.search_frame, font=("Times New Roman", 20), text="Molecular weight: ")
-        self.molecular_weight_label.config(bg="#b6d6fd")
-        self.molecular_weight_label.grid(row=13, column=8, padx=10, pady=5)
-
+        # molecular formula text
         self.molecular_formula = tk.Text(self.search_frame, width=10, height=1, font=("Times New Roman", 20))
         self.molecular_formula.grid(row=12, column=9, padx=10, pady=5)
         self.molecular_formula.insert(tk.END, str(results_from_pubchem[self.result_list.curselection()[0]].molecular_formula))
         self.molecular_formula.config(state=tk.DISABLED)
 
+        # molecular weight label
+        self.molecular_weight_label = tk.Label(self.search_frame, font=("Times New Roman", 20), text="Molecular weight (g/mol): ")
+        self.molecular_weight_label.config(bg="#b6d6fd")
+        self.molecular_weight_label.grid(row=13, column=8, padx=10, pady=5)
+
+        # molecular weight text
         self.molecular_weight = tk.Text(self.search_frame, width=10, height=1, font=("Times New Roman", 20))
         self.molecular_weight.grid(row=13, column=9, padx=10, pady=5)
         self.molecular_weight.insert(tk.END, str(results_from_pubchem[self.result_list.curselection()[0]].molecular_weight))
